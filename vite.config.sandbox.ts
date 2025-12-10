475dfdb0de72006d9a3790ada88620d44b9e9572
Anthony Mistretta <anthony@Anthonys-MacBook-Pro.local>
2025-12-10 13:31:57 -0700

    Prepare for sandbox setup

---
+++ vite.config.sandbox.ts
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv('sandbox', process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    }
+  }
+})
